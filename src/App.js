fa562d8df2b5931a8c69f276cf8a77d1e3f48205
Ino_A <>
2021-04-21 19:12:01 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import PersonForm from './components/PersonForm';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <PersonForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
